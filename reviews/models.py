a4abba4f1fef0a9ba817c66a990a7d7cfdc5e690
m.chizari <s.mohammadreza.lol@gmail.com>
2024-12-15 12:20:42 +0330

    m

--- reviews/models.py
+++ reviews/models.py
@@ -7,15 +7,12 @@ class Movie(models.Model):
     genre=models.CharField(max_length=258)
     release_date = models.DateField()
     director =models.CharField(max_length=258)
-    
     def __str__(self):
         return self.title
-    
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User,  on_delete=models.CASCADE)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
     def __str__(self):
         return f'{self.user.username} - {self.movie.title}'
--- reviews/serializers.py
+++ reviews/serializers.py
@@ -11,13 +11,11 @@ class ReviewSerializer(serializers.ModelSerializer):
     movie = serializers.SerializerMethodField()
     director = serializers.SerializerMethodField()
     user = serializers.SerializerMethodField()
-    
     class Meta:
         model= Review
         fields = ['movie','director','comment', 'user']
     def get_movie(self,obj):
         return obj.movie.title
-    
     def get_director(self,obj):
         return obj.movie.director
     
@@ -28,7 +26,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username','password','first_name','last_name','email')
-
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
