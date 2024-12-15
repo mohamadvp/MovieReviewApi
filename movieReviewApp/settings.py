2c7add5685144536853ec83d74840db3439b8bdc
m.chizari <s.mohammadreza.lol@gmail.com>
2024-12-15 11:23:10 +0330

    m

--- movieReviewApp/settings.py
+++ movieReviewApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!9(r)x--msvn$(a+@=neplp=ieo7v$bpx*qbv@5aqkc=skhp01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
